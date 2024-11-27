16498b00ecc7038119daf07d77f99cee6958f4c4
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-11-26 22:41:51 -0500

    Added flake.nix and .gitignore

---
+++ flake.nix
@@ -0,0 +1,37 @@
+# This file is part of aplwiz.
+#
+# Copyright (c) 2024 ona-li-toki-e-jan-Epiphany-tawa-mi
+#
+# aplwiz is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# aplwiz is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# aplwiz. If not, see <https://www.gnu.org/licenses/>.
+
+{
+  description = "aplwiz development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let inherit (nixpkgs.lib) genAttrs systems;
+
+        forAllSystems = f: genAttrs systems.flakeExposed (system: f {
+          pkgs = import nixpkgs { inherit system; };
+        });
+    in {
+      devShells = forAllSystems ({ pkgs }: {
+        default = with pkgs; mkShell {
+          nativeBuildInputs = [
+            gnuapl
+          ];
+        };
+      });
+    };
+}
